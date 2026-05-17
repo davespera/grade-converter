f838545bc88217c7b717796390488581a2e09318
David Esperante <davespera@proton.me>
2026-05-17 21:09:27 +0200

    fix: add api url

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,8 +1,14 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import { client } from '$lib/api/client.gen'
 
 	let { children } = $props();
+
+	// Point to backend
+	client.setConfig({
+    baseUrl: 'http://localhost:8000'
+  });
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
